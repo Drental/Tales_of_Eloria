92e1d3ebbdb066c106a94917fe6373a5a20ce557
Zolthex <71838114+Zolthex@users.noreply.github.com>
2021-09-06 16:31:48 +0200

    ... 2

---
+++ module/toeloria.js
@@ -0,0 +1,8 @@
+import toeloriaItemSheet form "./module/sheets/toeloriaItemSheet.js";
+
+Hooks.once("init", function(){
+	console.log("toeloria | Initialising Tales of Eloria System");
+	
+	Items.unregisterSheet("core", ItemSheet);
+	Items.registerSheet("toeloria", toeloriaItemSheet, { makeDefault: true });
+});
--- toeloria.js
+++
@@ -1,8 +0,0 @@
-import toeloriaItemSheet form "./module/sheets/toeloriaItemSheet.js";
-
-Hooks.once("init", function(){
-	console.log("toeloria | Initialising Tales of Eloria System");
-	
-	Items.unregisterSheet("core", ItemSheet);
-	Items.registerSheet("toeloria", toeloriaItemSheet, { makeDefault: true });
-});
